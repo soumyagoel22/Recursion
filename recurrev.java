fece581b19f63da7760b23712006ef459649446b
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-10-29 17:43:28 +0530

    Add files via upload

---
+++ recurrev.java
@@ -0,0 +1,24 @@
+package june21;
+
+public class recurrev {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr={55,11,44,22,33};
+		rev(arr,0);
+
+	}
+	public static void rev(int[] arr,int vidx)
+	{
+		if(vidx==arr.length)
+		{
+			return;
+		}
+		rev(arr,vidx+1);
+		System.out.println(arr[vidx]);
+	}
+
+}
